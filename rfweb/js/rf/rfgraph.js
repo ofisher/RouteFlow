017df1563bd37963966c441aa6c9d89b65279e9a
Allan Vidal <allanv@cpqd.com.br>
2012-04-04 16:07:39 -0300

    - Network view in rfweb now uses a new Jit graph.

--- rfweb/js/rf/rfgraph.js
+++ rfweb/js/rf/rfgraph.js
@@ -1,5 +1,12 @@
 var labelType, useGradients, nativeTextSupport, animate;
 
+ICON_SIZE = 64;
+LABEL_SIZE = 12;
+SPACING = 12;
+
+RFSWITCH_ICON = new Image();
+RFSWITCH_ICON.src = 'images/eth.png';
+
 (function() {
   var ua = navigator.userAgent,
       iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
@@ -14,80 +21,201 @@ var labelType, useGradients, nativeTextSupport, animate;
   useGradients = nativeCanvasSupport;
   animate = !(iStuff || !nativeCanvasSupport);
 })();
-
-
-
+            
 $jit.RGraph.Plot.NodeTypes.implement({
-    'rfswitch': {
+    'of-switch': {
         'render': function(node, canvas) {
-            var img = new Image();
-            img.src = 'images/rfswitch.png';
             var pos = node.pos.getc(true);
-            img.onload = function () {
-                canvas.getCtx().drawImage(img, pos.x - 32, pos.y - 32, 64, 64);
-            };
+            canvas.getCtx().drawImage(RFSWITCH_ICON, pos.x - ICON_SIZE/2, pos.y - ICON_SIZE/2, ICON_SIZE, ICON_SIZE);
         },
         
         'contains': function (node, pos) {
-            return this.nodeHelper.rectangle.contains(node.pos.getc(true), pos, 64, 64);
+            return this.nodeHelper.rectangle.contains(node.pos.getc(true), pos, ICON_SIZE, ICON_SIZE);
+        }
+    },
+    
+    'rf-server': {
+        'render': function(node, canvas) {
+            var pos = node.pos.getc(true);
+            canvas.getCtx().drawImage(RFSWITCH_ICON, pos.x - ICON_SIZE/2, pos.y - ICON_SIZE/2, ICON_SIZE, ICON_SIZE);
+        },
+        
+        'contains': function (node, pos) {
+            return this.nodeHelper.rectangle.contains(node.pos.getc(true), pos, ICON_SIZE, ICON_SIZE);
+        }
+    },
+
+    'controller': {
+        'render': function(node, canvas) {
+            var pos = node.pos.getc(true);
+            canvas.getCtx().drawImage(RFSWITCH_ICON, pos.x - ICON_SIZE/2, pos.y - ICON_SIZE/2, ICON_SIZE, ICON_SIZE);
+        },
+        
+        'contains': function (node, pos) {
+            return this.nodeHelper.rectangle.contains(node.pos.getc(true), pos, ICON_SIZE, ICON_SIZE);
         }
     },
 });
 
-function init(){
-    var json = [
-    {
-        "id": "rfcontroller",
-        "name": "RFController",
-        "adjacencies": [{"nodeTo": "rfserver"},
-                        {"nodeTo": "switch5"},
-                        {"nodeTo": "switch6"},
-                        {"nodeTo": "switch7"},
-                        {"nodeTo": "switch8"}]
-    },
+function show_info(graph, node) {
+    var contMatch = new Array();
+    var contActions = new Array();
+    var flowsActions = new Array();
+	
+	// Find the switch info
+	var sw = undefined;
+	for (var i in switches_info) {
+	    if (switches_info[i]["id"] == node.id) {
+	        sw = switches_info[i];
+	        break;
+	    }
+	}
+	if (sw == undefined)
+	    return;
 
-    {
-        "id": "rfserver",
-        "name": "RFServer",
-    },
+	// Build the right column statistics
+	var html = "<div class='section'><div class='section_title'>" + node.name + "</div>";
+	
+	//ofp_desc_stats
+	var switchData = sw["data"]['$ofp_desc_stats'];
+	if(switchData) {
+		html += "<div class='subsection'><div class='subsection_title'>Description</div><ul class=\"switchdesc\"><li>", list = [];
+		list.push("Manufacturer: " + switchData[0]);
+		list.push("Hardware descrpition: " + switchData[1]);
+		list.push("Software description: " + switchData[2]);
+		list.push("Serial number: " + switchData[3]);
+		list.push("Datapath description: " + switchData[4]);
+		html = html + list.join("</li><li>") + "</li></ul></div>";
+	
+	
+	//ofp_aggregate_stats
+	switchData = sw["data"]['$ofp_aggr_stats'];
+	if(switchData) {
+		html = html + "<div class='subsection'><div class='subsection_title'>Aggregated statistics</div><ul class=\"switchdesc\"><li>", list = [];
+		list.push("Packet count: " + switchData[0]);
+		list.push("Byte count: " + switchData[1]);
+		list.push("Flow count: " + switchData[2]);
+		html = html + list.join("</li><li>") + "</li></ul></div>";
+	}
+	
+	//ofp_table_stats
+	switchData = sw["data"]['$ofp_table_stats'];
+	html = html + "<div class='subsection'><div class='subsection_title'>Table statistics</div><ul class=\"switchdesc\"><li>", list = [];
+	if(switchData) {
+		list.push("Table ID: " + switchData[0][0]);
+		list.push("Name: " + switchData[0][1]);
+		list.push("Active count: " + switchData[0][2]);
+		list.push("Lookup count: " + switchData[0][3]);
+		html = html + list.join("</li><li>") + "</li></ul></div>";
+	}
+	html += "</div>"
+	// display information
+	$('#switchinfo').html(html);
+	
+	// Build the bottom flow table
+	var flows = sw["data"]['$flows'];		
+	html = "<table>" +
+	    "<tr class='header'>" +
+		    "<td>#</td>" +
+		    "<td>Matches</td>" +
+		    "<td>Actions</td>" +
+		    "<td>Packets</td>" +
+		    "<td>Bytes</td>" +
+	    "</tr>";
+	var matches = "";
 
-    {
-        "id": "switch5",
-        "name": "A",
-        "adjacencies": [{"nodeTo": "switch6"},
-                        {"nodeTo": "switch7"},
-                        {"nodeTo": "switch8"}],
-    },
+    var rowtemplate = "<tr class=\"bg\{style}\">"
+    rowtemplate += "<td class=\"{status}\">{flow}</td>"
+    rowtemplate += "<td>{matches}</td>"
+    rowtemplate += "<td>{actions}</td>"
+    rowtemplate += "<td>{packets}</td>"
+    rowtemplate += "<td>{bytes}</td>"
+    rowtemplate += "</tr>"
+    
+    var count = 0;
+    var status = "";
+	for(var i in flows) {
+		var f = flows[i];
+		
+		var actions = f.ofp_actions[0];
+		for (j in f.ofp_actions) {
+			if(j!=0 && f.ofp_actions[j] != 'type' && f.ofp_actions[j] != 'len')
+				actions = actions + f.ofp_actions[j];
+		}
+		if(tableMatches[node.id]!=undefined && tableMatches[node.id].search(f.ofp_match)!=-1) {
+			//fluxo ja estah na flow table mas eh recente
+			if(f.ofp_match in contMatch && contMatch[f.ofp_match] > 0) {
+                --contMatch[f.ofp_match];
+                status = "flow-kinda-new";
+			// fluxo estah na flow table ha tempo
+            } else {
+				delete contMatch[f.ofp_match];
+				// as acoes para este fluxo mudaram
+				if(flowsActions && flowsActions[f.ofp_match] != undefined && flowsActions[f.ofp_match] != actions) {
+						contActions[f.ofp_match] = 5;
+					    status = "flow-action-changed";
+				} else {
+					if(contActions[f.ofp_match] && contActions[f.ofp_match] > 0) {
+                        --contActions[f.ofp_match];
+                    }
+                    if(contActions[f.ofp_match] > 0) {
+                        status = "flow-action-changed";
+                    } else {
+						delete contActions[f.ofp_match];
+					}
+				}
+			}
+		// fluxo acaba de entrar na flow table
+		} else {
+			contMatch[f.ofp_match] = 5;
+			status = "flow-new";
+		}
+		flowsActions[f.ofp_match] = actions;
+		matches = matches + f.ofp_match;
 
-    {
-        "id": "switch6",
-        "name": "B",
-        "adjacencies": [{"nodeTo": "switch5"},
-                        {"nodeTo": "switch8"}],
-    },
+        var data = {
+            "flow": f.flow,
+            "matches": f.ofp_match,
+            "actions": actions,
+            "packets": f.packet_count,
+            "bytes": f.byte_count,
+            "status": status,
+            "style": count % 2,
+        }
+        html += apply_template(rowtemplate, data);
+		count = count + 1;
+	}
+	tableMatches[node.id] = matches;
+	html = html + "</table>";
+	
+	$('#flows').html(html);
+	}
+}
 
-    {
-        "id": "switch7",
-        "name": "C",
-        "adjacencies": [{"nodeTo": "switch5"},
-                        {"nodeTo": "switch8"}],
-    },
+var rgraph = undefined;
+var switches_info;
+var old_nodes;
+var old_topology = {};
+var current_topology;
+var interval_id;
+var tableMatches = {};
 
-    {
-        "id": "switch8",
-        "name": "D",
-        "adjacencies": [{"nodeTo": "switch5"},
-                        {"nodeTo": "switch6"},
-                        {"nodeTo": "switch7"}],
-    }];
+function start_updating() {
+    interval_id = setInterval("update()", 1000);
+}
 
-    var rgraph = new $jit.RGraph({
+function stop_updating() {
+    clearInterval(interval_id);
+}
+
+function build() {
+    rgraph = new $jit.RGraph({
       'injectInto': 'infovis',
         Node: {
             'type': 'rfswitch',
             'overridable': true,
             'color': '#cc0000',
-            'dim': '20',
+            'height': ICON_SIZE + SPACING + LABEL_SIZE,
         },
 
         Edge: {
@@ -101,38 +229,46 @@ function init(){
           zooming: 100,
         },
 
-        interpolation: 'polar',
+        Label: {  
+            overridable: false,  
+            type: labelType,
+            style: "bold",
+            size: 12,
+            family: 'sans-serif',  
+            textAlign: 'center',  
+            color: '#000'  
+        },
 
+        interpolation: 'polar',
         transition: $jit.Trans.linear,
         duration: 250,
         fps: 30,
         levelDistance: 200,
-
+        
         Events: {
             enable: true,
             type: 'Native',
 
             onDragMove: function(node, eventInfo, e) {
+                stop_updating();
                 var pos = eventInfo.getPos();
                 node.pos.setc(pos.x, pos.y);
                 rgraph.plot();
+                start_updating();
             },
 
             onClick: function(node, eventInfo, e) {
-                if (node)
-                    rgraph.onClick(node.id, { hideLabels: false });
+                if (node) {
+                    show_info(rgraph, node);
+                }
             },
 
             onRightClick: function(node, eventInfo, e) {
                 if (node) {
-                    var html = "<h4>" + node.name + "</h4><b>Connections:</b>";
-                    html += "<ul>";
-                    node.eachAdjacency(function(adj){
-                        var child = adj.nodeTo;
-                        html += "<li>" + child.name + "</li>";
-                    });
-                    html += "</ul>";
-                    $jit.id('inner-details').innerHTML = html;
+                    stop_updating();
+                    old_topology = {};
+                    rgraph.onClick(node.id, { hideLabels: false });
+                    start_updating();
                 }
             },
         },
@@ -142,16 +278,51 @@ function init(){
             domElement.setAttribute("class", "label");
         },
 
-        onPlaceLabel: function(domElement, node){
+        onPlaceLabel: function(domElement, node) {
             var style = domElement.style;
             var left = parseInt(style.left);
             var top = parseInt(style.top);
             var w = domElement.offsetWidth;
             style.left = (left - w / 2) + 'px';
-            style.top = (top + 10) + 'px';
+            style.top = top + (ICON_SIZE/2) + 'px';
         },
+
+        
     });
-    rgraph.loadJSON(json, 1);
-    rgraph.refresh();
-    rgraph.onClick("rfcontroller", { hideLabels: false });
+}
+
+function update() {
+    $.getJSON("data/topology.json",
+        function(data) {
+            if (rgraph == undefined) {
+                build();
+                rgraph.canvas.scale(0.8, 0.8);
+            }
+            
+            // Save old node positions
+            rgraph.graph.eachNode(function(node) {
+                old_topology[node.id] = node.getPos();
+            });
+            
+            // Update
+            rgraph.loadJSON(data["nodes"], 1);
+            rgraph.refresh();
+            
+            // Restore old positions
+            rgraph.graph.eachNode(function(node) {
+                if (node.id in old_topology)
+                    node.setPos(old_topology[node.id]);
+            });
+            rgraph.plot();
+    });
+    
+    $.getJSON("data/switchstats.json",
+        function(data) {
+            switches_info = data["nodes"];
+    });
+}
+
+function init() {
+    update();
+    start_updating();
 }
